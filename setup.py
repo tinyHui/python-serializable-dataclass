e64257a9693f666df2f67f555dd6a1a76e0e2798
Jason Chen <jiahuic@twitter.com>
2021-10-15 15:58:39 +0100

    Release 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="python-serializable-dataclass",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(exclude=("tests*",)),
     package_data={"lib": ["py.typed"]},
     author="jason",
